3f6fdd0209aa0bfba07e34ccef1d8d4409f583f0
Saidikrom <yusupovsaidikrom9@gmail.com>
2024-10-21 17:37:14 +0500

    api key secured

--- lib/core/constants/urls_const.dart
+++ lib/core/constants/urls_const.dart
@@ -2,5 +2,4 @@ class UrlsConst {
   // ignore: non_constant_identifier_names
   static String base_url = "https://api.weatherapi.com/v1/";
 
-  static String apiKey = 'da15e87bc8ba409aa95192108241410';
 }
